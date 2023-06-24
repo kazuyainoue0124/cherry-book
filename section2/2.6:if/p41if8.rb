e453ecb5785f861ce1763f8e6b57a94f21c4fd60
Kazuya INOUE <bauer24tbp@gmail.com>
2023-06-24 18:14:28 +0900

    p41ifやelsifの後ろにthenを入れると後続の処理を1行で書ける。使用頻度は低いらしい。

---
+++ section2/2.6:if/p41if8.rb
@@ -0,0 +1,10 @@
+country = 'italy'
+
+greeting = 
+    if country == 'japan' then 'こんにちは'
+    elsif country == 'us' then 'Hello'
+    elsif country == 'italy' then 'Ciao'
+    else '???'
+    end
+
+puts greeting
