bc6352af808d95c7527a19a1cef70678a734589a
Kazuya INOUE <bauer24tbp@gmail.com>
2023-07-04 08:28:25 +0900

    p61&&を使う場合はメソッド引数に括弧をつければOK

--- section2/2.10:more_about_boolean_and_conditional/p60operators_with_lower_precedence5.rb
+++ section2/2.10:more_about_boolean_and_conditional/p60operators_with_lower_precedence5.rb
@@ -3,3 +3,6 @@ user.valid? && send_mail_to user
 
 # andを使うと(user.valid?) and (send_mail_to user)と解釈される
 user.valid? and send_mail_to user
+
+# &&を使う場合はメソッドの引数に括弧をつければOK
+user.valid? && send_mail_to(user)
