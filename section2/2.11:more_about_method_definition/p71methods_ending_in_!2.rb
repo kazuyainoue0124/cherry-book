8098ffee25ebbee1c9aa095508945ff9e7025c48
Kazuya INOUE <bauer24tbp@gmail.com>
2023-07-09 14:21:33 +0900

    p71!で始まるメソッドも自分で定義できる。変数の内容自体を変えてしまうことを破壊的な変更という

---
+++ section2/2.11:more_about_method_definition/p71methods_ending_in_!2.rb
@@ -0,0 +1,21 @@
+# 引数の内容を変更しない安全バージョン
+def reverse_upcase(s)
+    s.reverse.upcase
+end
+
+# 引数の内容を破壊的に変更してしまう危険バージョン
+def reverse_upcase!(s)
+    s.reverse!
+    s.upcase!
+    s
+end
+
+s = 'ruby'
+
+# 安全バージョンは引数として渡した変数sの内容はそのまま
+puts reverse_upcase(s)
+puts s
+
+# 危険バージョンは引数として渡した変数sの内容が変更される
+puts reverse_upcase!(s)
+puts s
