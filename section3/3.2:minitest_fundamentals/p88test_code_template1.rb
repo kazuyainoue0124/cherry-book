508b63fb2bbe3b4802b89848c322a2d5a109bcdb
Kazuya INOUE <bauer24tbp@gmail.com>
2023-07-14 08:22:14 +0900

    ファイル名とディレクトリ移動

--- section3/3.2:minitest_fundamentals/p88test_code_template1.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-
-# 本来はクラス名SampleTestに合わせてファイル名はsample_test.rbとするべき
-class SampleTest < Minitest::Test
-  # Minitestはtest_で始まるメソッドを探して実行する
-  def test_sample
-    assert_equal 'RUBY', 'ruby'.upcase
-  end
-end
