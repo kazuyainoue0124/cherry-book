f0ae3bcaf8d497cced14a108f4d31cf180c25d0b
Kazuya INOUE <bauer24tbp@gmail.com>
2023-07-14 08:27:22 +0900

    p91テストコード実行中にエラーが発生する例

--- section3/sample_test.rb
+++ section3/sample_test.rb
@@ -4,7 +4,10 @@ require 'minitest/autorun'
 class SampleTest < Minitest::Test
   # Minitestはtest_で始まるメソッドを探して実行する
   def test_sample
+    # assert_equal 'RUBY', 'ruby'.upcase
     # わざとcapitalizeメソッド（最初の1文字だけ大文字にするメソッド）を呼ぶ
-    assert_equal 'RUBY', 'ruby'.capitalize
+    # assert_equal 'RUBY', 'ruby'.capitalize
+    # nilは文字列ではないので、upcaseメソッドを呼ぶことはできない
+    assert_equal 'RUBY', nil.upcase
   end
 end
