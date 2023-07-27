c52f94db9708fc6aaf792732cf39a2c5fb55b826
Kazuya INOUE <bauer24tbp@gmail.com>
2023-07-27 18:37:03 +0900

    p127mapmメソッドを使った実装にリファクタリング

--- section4/4.6:practice/lib/rgb.rb
+++ section4/4.6:practice/lib/rgb.rb
@@ -1,3 +1,5 @@
+# to_hexメソッド
+
 # リファクタリング前
 # def to_hex(r, g, b)
 #   '#' + 
@@ -22,14 +24,28 @@ def to_hex(r, g, b)
   end
 end
 
+
+# to_intsメソッド
+# リファクタリング前
+# def to_ints(hex)
+#   r = hex[1..2]
+#   g = hex[3..4]
+#   b = hex[5..6]
+#   ints = []
+
+#   [r, g, b].each do |s|
+#     ints << s.hex
+#   end
+#   ints
+# end
+
+# リファクタリング①
 def to_ints(hex)
   r = hex[1..2]
   g = hex[3..4]
   b = hex[5..6]
-  ints = []
-  
-  [r, g, b].each do |s|
-    ints << s.hex
+
+  [r, g, b].map do |s|
+    s.hex
   end
-  ints
 end
