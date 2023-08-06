604ce2290a2bb12d09eaf5cac36d56173f4c39eb
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-06 13:08:31 +0900

    p189ハッシュの前に**を付けてメソッドの引数に使えば、Ruby3.0以降でもハッシュがキーワード引数に変換される

--- section5/5.6:more_about_hash_and_keyword_arguments/p189convert_a_hash_into_keyword_arguments_explicitly_using_**1.rb
+++ section5/5.6:more_about_hash_and_keyword_arguments/p189convert_a_hash_into_keyword_arguments_explicitly_using_**1.rb
@@ -5,7 +5,11 @@ end
 
 # キーワード引数として渡したいハッシュを定義する
 params = { drink: true, potato: false }
+
 # ハッシュを引数として渡すと自動的にキーワード引数に変換される（Ruby2.x）
 # Ruby3.0以降は自動変換が行われないためエラーが発生する（キーワード引数の分離と呼ばれる）
 buy_burger('fish', params)
 #=> `buy_burger': wrong number of arguments (given 2, expected 1) (ArgumentError)
+
+## **付きでハッシュを渡すと、ハッシュがキーワード引数として扱われるようになる（警告やエラーが出ない）
+buy_burger('fish', **params)
