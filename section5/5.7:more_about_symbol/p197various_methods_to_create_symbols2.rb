e4dc20f66b38eca0232b4b134e429a4e76c1eec1
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-06 16:39:04 +0900

    p197識別子として無効な文字列を使う場合でもシングルクオートで囲まれていればシンボルとして有効になる

--- section5/5.7:more_about_symbol/p197various_methods_to_create_symbols2.rb
+++ section5/5.7:more_about_symbol/p197various_methods_to_create_symbols2.rb
@@ -1,6 +1,13 @@
 # 数字で始まったりスペースやハイフンが含まれる場合はエラー
+
 # 以下のようにシンボルを作ろうとするとエラーになる
 :12345       #=> SyntaxError
 :ruby-is-fun #=> NameError
 :ruby is fun #=> SyntaxError
 :()          #=> SyntaxError
+
+# ただし上記の文字列でもシングルクオートで囲むとシンボルとして有効になる
+:'12345'
+:'ruby-is-fun'
+:'ruby is fun'
+:'()'
