bc2b36de6f551769d299566712ac3cec470dc223
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-11 09:35:48 +0900

    p227Regexp.newを使って正規表現オブジェクトを作成する場合、mオプションの代わりに第2引数にRegexp::MULTILINEという定義を渡す

--- section6/6.5:more_about_regular_expression_objects/p227options_when_creating_regular_expressions2.rb
+++ section6/6.5:more_about_regular_expression_objects/p227options_when_creating_regular_expressions2.rb
@@ -4,3 +4,7 @@ puts "Hello\nBye" =~ /Hello.Bye/  #=> nil
 
 # mオプションを付けると.が改行文字にもマッチする
 puts "Hello\nBye" =~ /Hello.Bye/m #=> 0
+
+# Regexp.newを使う場合は、Regexp::MULTILINEという定義を渡す
+regexp = Regexp.new('Hello.Bye', Regexp::MULTILINE)
+puts "Hello\nBye" =~ regexp       #=> 0
