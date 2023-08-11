b9747bc2078947441bad52091188545f687d0262
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-11 09:29:47 +0900

    p227Regexp.newを使って正規表現オブジェクトを作成する場合、iオプションの代わりに第2引数にRegexp::IGNORECASEという定義を渡す

--- section6/6.5:more_about_regular_expression_objects/p227options_when_creating_regular_expressions1.rb
+++ section6/6.5:more_about_regular_expression_objects/p227options_when_creating_regular_expressions1.rb
@@ -3,3 +3,7 @@ puts 'HELLO' =~ /hello/i   #=> 0
 
 # %rを使った場合も最後にオプションを付けられる
 puts 'HELLO' =~ %r{hello}i #=> 0
+
+# Regexp.newを使う場合は、Regexp::IGNORECASEという定義を渡す
+regexp = Regexp.new('hello', Regexp::IGNORECASE)
+puts 'HELLO' =~ regexp     #=> 0
