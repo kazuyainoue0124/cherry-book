c9627ab8ee9eb6ad45838b09f8f20c845858d237
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-12 09:50:33 +0900

    p234フルネームを作るメソッドがあると再利用できて便利

--- section7/7.2:basic_knowledge_of_object_oriented_programming/p234class_usage_comparison1.rb
+++ section7/7.2:basic_knowledge_of_object_oriented_programming/p234class_usage_comparison1.rb
@@ -5,9 +5,14 @@ users = []
 users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
 users << { first_name: 'Bob', last_name: 'Python', age: 30 }
 
+# 氏名を作成するメソッド
+def full_name(user)
+  "#{user[:first_name]} #{user[:last_name]}"
+end
+
 # ユーザのデータを表示する
 users.each do |user|
-  puts "氏名: #{user[:first_name]} #{user[:last_name]}、年齢: #{user[:age]}"
+  puts "氏名: #{full_name(user)}、年齢: #{user[:age]}"
 end
 #=> 氏名: Alice Ruby、年齢: 20
 #   氏名: Bob Python、年齢: 30
