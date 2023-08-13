137a8253410f6413dfb79fd3044b159fdeddf664
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-13 09:39:21 +0900

    p243インスタンス変数を外部から変更したい場合も変更用のメソッドをクラスに定義する必要がある

---
+++ section7/7.3:class_definition/p243instance_variables_and_accessors7.rb
@@ -0,0 +1,22 @@
+# インスタンス変数の内容を外部から変更したい場合も変更用のメソッドを定義する必要がある
+# Rubyでは=で終わるメソッドを定義すると変数に代入するような形式でメソッドを呼び出せる
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  # @nameを外部から参照するためのメソッド
+  def name
+    @name
+  end
+
+  # @nameを外部から変更するためのメソッド
+  def name=(value)
+    @name = value
+  end
+end
+
+user = User.new('Alice')
+# 変数に代入しているように見えるが、実際はname=メソッドを呼び出している
+user.name = 'Bob'
+puts user.name #=> "Bob"
