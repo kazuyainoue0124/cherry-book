2c9ea8489adb649513244b4dda02a509b9c73256
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-20 13:32:32 +0900

    p300クラス内で演算子をオーバーライドし挙動を再定義できる

---
+++ section7/7.10:advanced_topics/p300customizing_operator_behaviors2.rb
@@ -0,0 +1,31 @@
+# クラス内で演算子をオーバーライドできる
+class Product
+  attr_reader :code, :name
+
+  def initialize(code, name)
+    @code = code
+    @name = name
+  end
+
+  # == をProductクラスでオーバーライド
+  def ==(other)
+    # otherがProductかつ、商品コードが一致していれば同じProductと見なす
+    other.is_a?(Product) && code == other.code
+  end
+end
+
+# aとcが同じ商品コード
+a = Product.new('A-0001', 'A great movie')
+b = Product.new('B-0001', 'An awesome film')
+c = Product.new('A-0001', 'A great movie')
+
+# デフォルトでは同じobject_id(まったく同じインスタンス)の場合にtrueになる
+puts a == a   #=> true
+
+# 商品コードが一致すればtrueになる
+puts a == b   #=> false
+puts a == c   #=> true
+
+# Product以外の比較はfalse
+puts a == 1   #=> false
+puts a == 'a' #=> false
