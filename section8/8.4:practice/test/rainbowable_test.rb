7905d43fdce777709de6874ecdd4d7049e5079e5
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-24 08:27:00 +0900

    p327テストコードを実装しテスト失敗を確認

--- section8/8.4:practice/test/rainbowable_test.rb
+++ section8/8.4:practice/test/rainbowable_test.rb
@@ -2,8 +2,16 @@ require 'minitest/autorun'
 require_relative '../lib/rainbowable'
 
 class RainbowableTest < Minitest::Test
+  def setup
+    # 文字列や配列でrainbowメソッドが呼び出せるよう、Rainbowableモジュールをinclude
+    String.include.Rainbowable
+    Array.include.Rainbowable
+  end
   def test_rainbow
     # とりあえずモジュールが参照できることを確認する
-    assert Rainbowable
+    assert_equal expected, 'Hello, world!'.rainbow
+
+    expected = "\e[31mH\e[32me\e[33ml\e[34ml\e[35mo\e[36m,\e[31m \e[32mw\e[33mo\e[34mr\e[35ml\e[36md\e[31m!\e[0m"
+    assert_equal expected, [1, 2, 3].rainbow
   end
 end
