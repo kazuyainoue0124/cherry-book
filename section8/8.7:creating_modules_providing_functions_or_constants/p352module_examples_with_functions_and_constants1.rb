2e56267b6a6f2f2b61667d23f14cd3954b312ea9
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-29 07:10:57 +0900

    p352Mathモジュールには自然対数の底を表すEと円周率を表すPIが定義されている

--- section8/8.7:creating_modules_providing_functions_or_constants/p352module_examples_with_functions_and_constants1.rb
+++ section8/8.7:creating_modules_providing_functions_or_constants/p352module_examples_with_functions_and_constants1.rb
@@ -15,3 +15,7 @@ end
 
 calculator = Calculator.new
 puts calculator.calc_sqrt(2) #=> 1.4142135623730951
+
+# 自然対数の底を表すEと円周率を表すPIという定数が定義されている
+puts Math::E  #=> 2.718281828459045
+puts Math::PI #=> 3.141592653589793
