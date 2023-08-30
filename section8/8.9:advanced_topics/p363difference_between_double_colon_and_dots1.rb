59dfacf3ec2854913579f6a25731f725c794966d
Kazuya INOUE <bauer24tbp@gmail.com>
2023-08-31 08:30:45 +0900

    p363ドットの代わりに二重コロンでメソッドを呼び出すこともできる

--- section8/8.9:advanced_topics/p363difference_between_double_colon_and_dots1.rb
+++ section8/8.9:advanced_topics/p363difference_between_double_colon_and_dots1.rb
@@ -13,3 +13,6 @@ end
 # メソッドを呼び出すときはドットを使うのが典型的な使い分け
 puts Sample::User::NAME #=> "Alice"
 puts Sample::User.hello #=> "Hello, I am Alice."
+
+# でもメソッド呼び出しには二重コロンも使える
+puts Sample::User::hello #=> "Hello, I am Alice."
