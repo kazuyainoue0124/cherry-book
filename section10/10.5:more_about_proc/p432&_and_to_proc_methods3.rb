1f2f570406dd902a989c129a3b07090d074c128c
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-06 21:45:21 +0900

    p432シンボルを変換したProcオブジェクトに引数を1つ渡すと、1つ目の引数をレシーバにし、そのレシーバに対して元のシンボルと同じ名前のメソッドを呼び出す

--- section10/10.5:more_about_proc/p432&_and_to_proc_methods3.rb
+++ section10/10.5:more_about_proc/p432&_and_to_proc_methods3.rb
@@ -3,3 +3,8 @@
 # :splitというシンボルをto_procでProcオブジェクトに変換
 split_proc = :split.to_proc
 p split_proc #=> #<Proc:0x0000000102e90a90(&:split) (lambda)>
+
+# このProcオブジェクトに引数を1つ渡して実行すると、1つ目の引数をレシーバにし、
+# そのレシーバに対して元のシンボルと同じ名前のメソッドを呼び出す
+# 'a-b-c-d e'.splitと同じ(ホワイトスペースで分割する)
+p split_proc.call('a-b-c-d e') #=> ["a-b-c-d", "e"]
