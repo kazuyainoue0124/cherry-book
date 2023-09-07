cf9988ebcd96d93ed1c5b8250c7ad6cad03155a4
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-08 08:23:18 +0900

    p440パターンマッチを使って実装する例

---
+++ section11/11.2:pattern_match_fundamentals/p440fundamentals2.rb
@@ -0,0 +1,22 @@
+# パターンマッチを使うと次のように書ける
+records = [
+  [2021],
+  [2019, 5],
+  [2017, 11, 25],
+]
+
+require 'date'
+
+records.map do |record|
+  case record
+  in [y]
+    Date.new(y, 1, 1)
+  in [y, m]
+    Date.new(y, m, 1)
+  in [y, m, d]
+    Date.new(y, m, d)
+  end
+end
+#=> [#<Date: 2021-01-01 ((2459216j,0s,0n),+0s,2299161j)>,
+#    #<Date: 2019-05-01 ((2458605j,0s,0n),+0s,2299161j)>,
+#    #<Date: 2017-11-25 ((2458083j,0s,0n),+0s,2299161j)>]
