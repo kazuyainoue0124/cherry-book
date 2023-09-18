50571625161a9167dc7eceb42f0a32a1133641c7
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-19 08:44:02 +0900

    p523apメソッドで整形された配列の内容が出力

--- section13/13.9:gem_and_bundler/sample-project/sample.rb
+++ section13/13.9:gem_and_bundler/sample-project/sample.rb
@@ -1,6 +1,15 @@
 # bundle exec ruby sample.rbを実行するとGemfileで指定したバージョンが使われる
 # ruby sample.rbを実行するとGemfileではなくその環境にインストールされている一番新しいバージョンが使われる
 require 'faker'
+require 'awesome_print'
 
 puts Faker::VERSION   #=> 2.16.0
 puts Faker::Name.name #=> "Penelope Leannon"
+
+# apはAwesome Printによって追加されるターミナル出力メソッド
+ap ['Alice', 'Bob', 'Carol']
+#=> [
+#     [0] "Alice",
+#     [1] "Bob",
+#     [2] "Carol"
+#   ]
