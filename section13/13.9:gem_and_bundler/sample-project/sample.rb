d54f7166dc07ee5d9fdb704f8a5b687493297f42
Kazuya INOUE <bauer24tbp@gmail.com>
2023-09-19 08:35:32 +0900

    p522bundle execを付けるとGemfileで指定したバージョンが使われる。付けない場合はその環境にインストールされている一番新しいバージョンが使われる

--- section13/13.9:gem_and_bundler/sample-project/sample.rb
+++ section13/13.9:gem_and_bundler/sample-project/sample.rb
@@ -1,4 +1,6 @@
+# bundle exec ruby sample.rbを実行するとGemfileで指定したバージョンが使われる
+# ruby sample.rbを実行するとGemfileではなくその環境にインストールされている一番新しいバージョンが使われる
 require 'faker'
 
-puts Faker::VERSION   #=> 3.2.1
+puts Faker::VERSION   #=> 2.16.0
 puts Faker::Name.name #=> "Penelope Leannon"
